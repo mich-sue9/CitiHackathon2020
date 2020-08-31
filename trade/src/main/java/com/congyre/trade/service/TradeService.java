13259f9a2c7ea685f3b7c5870bdae7de5e773d79
Michelle Lim <mich.sue9@gmail.com>
2020-08-31 15:16:33 +0000

    Update Trade Service to include more features

--- trade/src/main/java/com/congyre/trade/service/TradeService.java
+++ trade/src/main/java/com/congyre/trade/service/TradeService.java
@@ -3,45 +3,44 @@ import com.congyre.trade.entity.Trade;
 import com.congyre.trade.repository.TradeRepository;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TradeService {
 
+    @Autowired
     private TradeRepository repo;
 
 
     public Collection<Trade> getAllTrade(){
-        //TODO:ADD 
         return repo.findAll();
     }
     public void addTrade (Trade trade){
-        
-        //TODO:ADD 
         repo.insert(trade);
     }
     public void deleteTradeById(ObjectId id){
-
-        //TODO:ADD 
-        return;
-
-
+        repo.deleteById(id);
     }
+
     public Optional<Trade> getTradeById(ObjectId id){
-        //TODO:ADD
-        return null;
+        return repo.findById(id);
     }
     public void updateTrade(Trade trade){
         //TODO:ADD
-
 
         return;
     }
 
     public Collection<Trade> returnTrades(String name){
         //
+        return null;
+    }
+
+    public Iterable<Trade> getTradeByTicker(String tradeName){
         return null;
     }
 
