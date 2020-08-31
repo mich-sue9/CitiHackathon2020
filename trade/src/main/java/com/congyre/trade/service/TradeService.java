5c5480f1be09aa6bb4825f7fe2402e240d0d952d
chloe <gengxue0221@gmail.com>
2020-08-31 15:03:46 +0000

    commit service

--- trade/src/main/java/com/congyre/trade/service/TradeService.java
+++ trade/src/main/java/com/congyre/trade/service/TradeService.java
@@ -1,5 +1,7 @@
 package com.congyre.trade.service;
 import com.congyre.trade.entity.Trade;
+import com.congyre.trade.repository.TradeRepository;
+
 import java.util.Collection;
 
 import org.bson.types.ObjectId;
@@ -7,15 +9,25 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TradeService {
+
+    private TradeRepository repo;
+
+
     public Collection<Trade> getAllTrade(){
         //TODO:ADD 
-        return null;
+        return repo.findAll();
     }
     public void addTrade (Trade trade){
+        
         //TODO:ADD 
+        repo.insert(trade);
     }
     public void deleteTradeById(ObjectId id){
+
         //TODO:ADD 
+        return;
+
+
     }
     public Optional<Trade> getTradeById(ObjectId id){
         //TODO:ADD
@@ -23,6 +35,9 @@ public class TradeService {
     }
     public void updateTrade(Trade trade){
         //TODO:ADD
+
+
+        return;
     }
 
     public Collection<Trade> returnTrades(String name){
