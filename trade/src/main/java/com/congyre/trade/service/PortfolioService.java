62e19756c827ab09a3409d8b248bfe2643c33364
Kexin Hu <kexin.hu@outlook.com>
2020-09-11 18:04:52 +0000

    portfolio service

---
+++ trade/src/main/java/com/congyre/trade/service/PortfolioService.java
@@ -0,0 +1,25 @@
+package com.congyre.trade.service;
+
+import com.congyre.trade.entity.Portfolio;
+import com.congyre.trade.repository.PortfolioRepository;
+
+import java.util.logging.Logger;
+
+import com.congyre.trade.repository.TradeRepository;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PortfolioService {
+    private static Logger log = Logger.getLogger(PortfolioService.class.getName());
+
+    @Autowired
+    private PortfolioRepository repo;
+
+    public void getportfolio(ObjectId userId){
+
+    }
+
+}
