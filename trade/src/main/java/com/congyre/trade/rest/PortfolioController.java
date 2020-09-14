312ee98516e39b0464194399495c246a68950b06
Wendy Bai <wendybai@hotmail.com>
2020-09-14 17:34:52 +0000

    update add portfolio

--- trade/src/main/java/com/congyre/trade/rest/PortfolioController.java
+++ trade/src/main/java/com/congyre/trade/rest/PortfolioController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -81,11 +82,15 @@ public class PortfolioController {
         service.removeStock(ticker, quantity, new ObjectId(portfolioId));
     }
 
-    /*** FOR TESTING PURPOSES */
-    @RequestMapping(method = RequestMethod.POST, value="/addPortfolio")
-	public void addPortfolio() {
-		service.addPortfolio();
-	}
+  
+    @RequestMapping(method = RequestMethod.POST, value="/addPortfolio/{userId}")
+    public void addPortfolio(@PathVariable ObjectId userId, @RequestBody Portfolio port){
+        service.addPortfolio(userId, port);
+    }
+      /*** FOR TESTING PURPOSES */
+	// public void addPortfolio() {
+	// 	service.addPortfolio();
+	// }
 }
 
 
