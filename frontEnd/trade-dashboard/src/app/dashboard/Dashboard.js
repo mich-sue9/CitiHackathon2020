e2181d5136eb5f7a17b7bc331dd1c410bba6a70a
chloe <gengxue0221@gmail.com>
2020-09-15 19:06:21 +0000

    Dashboard

--- frontEnd/trade-dashboard/src/app/dashboard/Dashboard.js
+++ frontEnd/trade-dashboard/src/app/dashboard/Dashboard.js
@@ -275,16 +275,20 @@ export class Dashboard extends Component {
           <div className="col-12 grid-margin">
             <div className="card">
               <div className="card-body">
-                <h4 className="card-title">Recent Tickets</h4>
+                <h4 className="card-title">Trade history</h4>
                 <div className="table-responsive">
                   <table className="table">
                     <thead>
                       <tr>
-                        <th> Assignee </th>
-                        <th> Subject </th>
+                        <th> Date </th>
+                        <th> Ticker </th>
+                        <th> Quantity </th>
+                        <th> price </th>
+                        <th> Type </th>
                         <th> Status </th>
-                        <th> Last Update </th>
-                        <th> Tracking ID </th>
+                        <th> Total investment </th>
+                        <th> Last Close </th>
+
                       </tr>
                     </thead>
                     <tbody>
@@ -330,10 +334,95 @@ export class Dashboard extends Component {
                       </tr>
                     </tbody>
                   </table>
+
+
+
+
+                  
                 </div>
               </div>
             </div>
           </div>
+
+
+      {/* second table */}
+          <div className="col-12 grid-margin">
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-title">Outstanding Trades</h4>
+                <div className="table-responsive">
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th> Date </th>
+                        <th> Ticker </th>
+                        <th> Quantity </th>
+                        <th> price </th>
+                        <th> Type </th>
+                        <th> Status </th>
+                        <th> Total investment </th>
+                        <th> Last Close </th>
+
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <img src={require("../../assets/images/faces/face1.jpg")} className="mr-2" alt="face" /> David Grey </td>
+                        <td> Fund is not recieved </td>
+                        <td>
+                          <label className="badge badge-gradient-success">DONE</label>
+                        </td>
+                        <td> Dec 5, 2017 </td>
+                        <td> WD-12345 </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img src={require("../../assets/images/faces/face2.jpg")} className="mr-2" alt="face" /> Stella Johnson </td>
+                        <td> High loading time </td>
+                        <td>
+                          <label className="badge badge-gradient-warning">PROGRESS</label>
+                        </td>
+                        <td> Dec 12, 2017 </td>
+                        <td> WD-12346 </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img src={require("../../assets/images/faces/face3.jpg")} className="mr-2" alt="face" /> Marina Michel </td>
+                        <td> Website down for one week </td>
+                        <td>
+                          <label className="badge badge-gradient-info">ON HOLD</label>
+                        </td>
+                        <td> Dec 16, 2017 </td>
+                        <td> WD-12347 </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img src={require("../../assets/images/faces/face4.jpg")} className="mr-2" alt="face" /> John Doe </td>
+                        <td> Loosing control on server </td>
+                        <td>
+                          <label className="badge badge-gradient-danger">REJECTED</label>
+                        </td>
+                        <td> Dec 3, 2017 </td>
+                        <td> WD-12348 </td>
+                      </tr>
+                    </tbody>
+                  </table>
+
+
+
+
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+
+
+          
         </div>
       </div> 
     );
