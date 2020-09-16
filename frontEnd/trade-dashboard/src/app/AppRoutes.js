b1ef3c69124093cc8f3a49766626b87ceaaacc97
Wendy Bai <wendybai@hotmail.com>
2020-09-16 19:56:33 +0000

    changed the folder name to trade-form

--- frontEnd/trade-dashboard/src/app/AppRoutes.js
+++ frontEnd/trade-dashboard/src/app/AppRoutes.js
@@ -5,7 +5,7 @@ import Spinner from '../app/shared/Spinner';
 
 const Dashboard = lazy(() => import('./dashboard/Dashboard'));
 
-const BasicElements = lazy(() => import('./form-elements/BasicElements'));
+const BasicElements = lazy(() => import('./trade-form/trade-form'));
 
 const BasicTable = lazy(() => import('./tables/BasicTable'));
 
@@ -22,7 +22,7 @@ class AppRoutes extends Component {
       <Suspense fallback={<Spinner/>}>
         <Switch>
           <Route exact path="/dashboard" component={ Dashboard } />
-          <Route path="/form-Elements/basic-elements" component={ BasicElements } />
+          <Route path="/trade-form/trade-form" component={ BasicElements } />
           <Route path="/tables/basic-table" component={ BasicTable } />
           <Route path="/error-pages/error-404" component={ Error404 } />
           <Route path="/error-pages/error-500" component={ Error500 } />
