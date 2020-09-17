50f910675203d52a8d6c51e6ea0b975d4ddfc8fe
Wendy Bai <wendybai@hotmail.com>
2020-09-17 18:09:05 +0000

    add test file

---
+++ trade/src/test/java/com/congyre/trade/ControllerTest/PortfolioControllerTest.java
@@ -0,0 +1,15 @@
+package com.congyre.trade.ControllerTest;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import org.junit.Test;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes=PortfolioControllerTest.Config.class)
+public class PortfolioControllerTest {
+    
+}
