766d6f10630b52b7c856386478d3041fdf55814f
Wendy Bai <wendybai@hotmail.com>
2020-09-17 00:23:26 +0000

    update bashboard with pie chart, get total expense

--- frontEnd/trade-dashboard/src/app/dashboard/Dashboard.js
+++ frontEnd/trade-dashboard/src/app/dashboard/Dashboard.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import {Bar, Doughnut} from 'react-chartjs-2';
-import {getPortfolio} from '../api/PortfolioAPI';
+import {getPortfolio,getStockList} from '../api/PortfolioAPI';
+import * as scale from "d3-scale";
 
 
-
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
 
 export class Dashboard extends Component {
   handleChange = date => {
@@ -16,57 +20,14 @@ export class Dashboard extends Component {
     super(props)
     this.state = {
       error: false,
-      portfolioId: "5f623103c7e8ad1aca356733",
+      portfolioId: "5f628e0a113d5610c2ccea84",
       liveData:[],
       valuation : "",
-      startDate: new Date(),
-      visitSaleData: {},
-      visitSaleOptions: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true,
-              display:false,
-              min: 0,
-              stepSize: 20,
-              max: 80
-            },
-            gridLines: {
-              drawBorder: false,
-              color: 'rgba(235,237,242,1)',
-              zeroLineColor: 'rgba(235,237,242,1)'
-            }
-          }],
-          xAxes: [{
-            gridLines: {
-              display:false,
-              drawBorder: false,
-              color: 'rgba(0,0,0,1)',
-              zeroLineColor: 'rgba(235,237,242,1)'
-            },
-            ticks: {
-              padding: 20,
-              fontColor: "#9c9fa6",
-              autoSkip: true,
-            },
-            categoryPercentage: 0.5,
-            barPercentage: 0.5
-        }]
-        },
-        legend: {
-          display: false,
-        },
-        elements: {
-          point: {
-            radius: 0
-          }
-        }
-      },
       portfolioData:"",
       loadPortfolio: false,
     } 
   }
-  
+
   //get the portfolioData upon load
   loadPortfolio(){
     //get the reponse from the backend
@@ -76,151 +37,70 @@ export class Dashboard extends Component {
     response
       .then(resp => resp.json())
       .then(
-        result => {
+        data => {
+          //console.log(data)
+          data.totalExpense = formatter.format(data.totalExpense);
           this.setState({
             loadPortfolio: true,
-            portfolioData: result,
+            portfolioData: data,
+            
           });
+          
         },
         err =>{
           this.setState({
             loadPortfolio:false,
           });
         }
-      );   
+      );  
   }
 
-  renderStockList(stocklist){
-    //let tableBody = stocklist.map((employee));
+  loadLiveStockPrice(){
+    //get the livestock price 
+   let response = getStockList(this.state.portfolioId);
+    response
+      .then(res => res.json())
+      .then( data => {
+      //  console.log(data)
+        this.setState(
+          {valuation: formatter.format(data.valuation),
+           liveData: data.stockPrice})
+      });
   }
+
   componentDidMount(){
-    //get portfoliodata
-    this.loadPortfolio();
+    
 
     // Retrieve stock profile valuation & stock prices 
     console.log("Component mounted");
-    var url = 
-    fetch('http://localhost:8080/'+'api/portfolios/getStockLivePrice/'+this.state.portfolioId, {
-      method: "GET"})
-        .then(res => res.json())
-        .then( data => {
-          var formatter = new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD',
-          });
-          this.setState({valuation: formatter.format(data.valuation),
-                          liveData: data.stockPrice});});
-        
-
-
-    //your code
-    var ctx = document.getElementById('visitSaleChart').getContext("2d")
-    var gradientBar1 = ctx.createLinearGradient(0, 0, 0, 181)
-    gradientBar1.addColorStop(0, 'rgba(218, 140, 255, 1)')
-    gradientBar1.addColorStop(1, 'rgba(154, 85, 255, 1)')
-
-    var gradientBar2 = ctx.createLinearGradient(0, 0, 0, 360)
-    gradientBar2.addColorStop(0, 'rgba(54, 215, 232, 1)')
-    gradientBar2.addColorStop(1, 'rgba(177, 148, 250, 1)')
-
-    var gradientBar3 = ctx.createLinearGradient(0, 0, 0, 300)
-    gradientBar3.addColorStop(0, 'rgba(255, 191, 150, 1)')
-    gradientBar3.addColorStop(1, 'rgba(254, 112, 150, 1)')
-
-    var gradientdonut1 = ctx.createLinearGradient(0, 0, 0, 181)
-    gradientdonut1.addColorStop(0, 'rgba(54, 215, 232, 1)')
-    gradientdonut1.addColorStop(1, 'rgba(177, 148, 250, 1)')
-
-    var gradientdonut2 = ctx.createLinearGradient(0, 0, 0, 50)
-    gradientdonut2.addColorStop(0, 'rgba(6, 185, 157, 1)')
-    gradientdonut2.addColorStop(1, 'rgba(132, 217, 210, 1)')
-
-    var gradientdonut3 = ctx.createLinearGradient(0, 0, 0, 300)
-    gradientdonut3.addColorStop(0, 'rgba(254, 124, 150, 1)')
-    gradientdonut3.addColorStop(1, 'rgba(255, 205, 150, 1)')
-
-
-
-    const newVisitSaleData = {
-      labels: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG'],
-      datasets: [
-        {
-          label: "CHN",
-          borderColor: gradientBar1,
-          backgroundColor: gradientBar1,
-          hoverBackgroundColor: gradientBar1,
-          legendColor: gradientBar1,
-          pointRadius: 0,
-          fill: false,
-          borderWidth: 1,
-          data: [20, 40, 15, 35, 25, 50, 30, 20]
-        },
-        {
-          label: "USA",
-          borderColor: gradientBar2,
-          backgroundColor: gradientBar2,
-          hoverBackgroundColor: gradientBar2,
-          legendColor: gradientBar2,
-          pointRadius: 0,
-          fill: false,
-          borderWidth: 1,
-          data: [40, 30, 20, 10, 50, 15, 35, 40]
-        },
-        {
-          label: "UK",
-          borderColor: gradientBar3,
-          backgroundColor: gradientBar3,
-          hoverBackgroundColor: gradientBar3,
-          legendColor: gradientBar3,
-          pointRadius: 0,
-          fill: false,
-          borderWidth: 1,
-          data: [70, 10, 30, 40, 25, 50, 15, 30]
-        }
-      ]
-    }
-    const newTrafficData = {
-      datasets: [{
-        data: [30, 30, 40],
-          backgroundColor: [
-            gradientdonut1,
-            gradientdonut2,
-            gradientdonut3
-          ],
-          hoverBackgroundColor: [
-            gradientdonut1,
-            gradientdonut2,
-            gradientdonut3
-          ],
-          borderColor: [
-            gradientdonut1,
-            gradientdonut2,
-            gradientdonut3
-          ],
-          legendColor: [
-            gradientdonut1,
-            gradientdonut2,
-            gradientdonut3
-          ]
-      }],
-  
-      // These labels appear in the legend and in the tooltips when hovering different arcs
-      labels: [
-        'Search Engines',
-        'Direct Click',
-        'Bookmarks Click',
-      ]
-    };
-    this.setState({visitSaleData: newVisitSaleData, trafficData:newTrafficData} )
+    //get portfoliodata
+    this.loadPortfolio();
+    this.loadLiveStockPrice();  
   }
 
-
-
-  toggleProBanner() {
-    document.querySelector('.proBanner').classList.toggle("hide");
-  }
 
   render () {
+    let l = this.state.liveData.length
+  
+    //create a color list
+    const colors = scale.scaleLinear().domain([0, l]).range(["#1D9A6C", "#3B73B4"]);
+    console.log(colors);
+
+    let colorList =[]
+    for(var i=0;i<l;i++){
+      colorList.push(colors(i))
+    }
+
+  
+    const newTrafficData = {
+      datasets: [{
+        data: this.state.liveData.map(s => s.quantity),
+        backgroundColor: colorList
+      }],
+      // These labels appear in the legend and in the tooltips when hovering different arcs
+      labels: this.state.liveData.map(s => s.ticker)
+    };
+    //create the pie chart
     return (
       <div>
         <div className="page-header">
@@ -237,7 +117,7 @@ export class Dashboard extends Component {
           </nav>
         </div>
         <div className="row">
-          <div className="col-md-4 stretch-card grid-margin">
+          <div className="col-md-6 stretch-card grid-margin">
             <div className="card bg-gradient-danger card-img-holder text-white">
               <div className="card-body">
                 <img src={require("../../assets/images/dashboard/circle.svg")} className="card-img-absolute" alt="circle" />
@@ -248,83 +128,19 @@ export class Dashboard extends Component {
               </div>
             </div>
           </div>
-          <div className="col-md-4 stretch-card grid-margin">
+          <div className="col-md-6 stretch-card grid-margin">
             <div className="card bg-gradient-info card-img-holder text-white">
               <div className="card-body">
                 <img src={require("../../assets/images/dashboard/circle.svg")} className="card-img-absolute" alt="circle" />
-                <h4 className="font-weight-normal mb-3">Weekly Orders <i className="mdi mdi-bookmark-outline mdi-24px float-right"></i>
+                <h4 className="font-weight-normal mb-3">Total Expense <i className="mdi mdi-bookmark-outline mdi-24px float-right"></i>
                 </h4>
-                <h2 className="mb-5">45,6334</h2>
-                <h6 className="card-text">Decreased by 10%</h6>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-4 stretch-card grid-margin">
-            <div className="card bg-gradient-success card-img-holder text-white">
-              <div className="card-body">
-                <img src={require("../../assets/images/dashboard/circle.svg")} className="card-img-absolute" alt="circle" />
-                <h4 className="font-weight-normal mb-3">Visitors Online <i className="mdi mdi-diamond mdi-24px float-right"></i>
-                </h4>
-                <h2 className="mb-5">95,5741</h2>
-                <h6 className="card-text">Increased by 5%</h6>
+                <h2 className="mb-5">{this.state.portfolioData.totalExpense}</h2>
               </div>
             </div>
           </div>
         </div>
         <div className="row">
           <div className="col-md-7 grid-margin stretch-card">
-            <div className="card">
-              <div className="card-body">
-                <div className="clearfix mb-4">
-                  <h4 className="card-title float-left">Visit And Sales Statistics</h4>
-                  <div id="visit-sale-chart-legend" className="rounded-legend legend-horizontal legend-top-right float-right">
-                    <ul>
-                      <li>
-                        <span className="legend-dots bg-primary">
-                        </span>CHN
-                      </li>
-                      <li>
-                        <span className="legend-dots bg-danger">
-                        </span>USA
-                      </li>
-                      <li>
-                        <span className="legend-dots bg-info">
-                        </span>UK
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <Bar ref='chart' className="chartLegendContainer" data={this.state.visitSaleData} options={this.state.visitSaleOptions} id="visitSaleChart"/>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-5 grid-margin stretch-card">
-            <div className="card">
-              <div className="card-body">
-                <h4 className="card-title">Traffic Sources</h4>
-                <Doughnut data={this.state.trafficData} options={this.state.trafficOptions} />
-                <div id="traffic-chart-legend" className="rounded-legend legend-vertical legend-bottom-left pt-4">
-                  <ul>
-                    <li>
-                      <span className="legend-dots bg-info"></span>Search Engines
-                      <span className="float-right">30%</span>
-                    </li>
-                    <li>
-                      <span className="legend-dots bg-success"></span>Direct Click
-                      <span className="float-right">30%</span>
-                    </li>
-                    <li>
-                      <span className="legend-dots bg-danger"></span>Bookmarks Click
-                      <span className="float-right">40%</span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12 grid-margin">
             <div className="card">
               <div className="card-body">
                 <h4 className="card-title">Live Stock Prices*</h4>
@@ -339,10 +155,6 @@ export class Dashboard extends Component {
                     </thead>
                     <tbody>
                         {this.state.liveData.map((stock)=> {
-                           var formatter = new Intl.NumberFormat('en-US', {
-                            style: 'currency',
-                            currency: 'USD',
-                          });
                           return(
                             <tr>
                               <td> {stock.ticker} </td>
@@ -356,86 +168,23 @@ export class Dashboard extends Component {
               </div>
             </div>
           </div>
-
-
-      {/* second table */}
-          <div className="col-12 grid-margin">
+          <div className="col-md-5 grid-margin stretch-card">
             <div className="card">
               <div className="card-body">
-                <h4 className="card-title">Outstanding Trades</h4>
-                <div className="table-responsive">
-                  <table className="table">
-                    <thead>
-                      <tr>
-                        <th> Date </th>
-                        <th> Ticker </th>
-                        <th> Quantity </th>
-                        <th> price </th>
-                        <th> Type </th>
-                        <th> Status </th>
-                        <th> Total investment </th>
-                        <th> Last Close </th>
-
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>
-                          <img src={require("../../assets/images/faces/face1.jpg")} className="mr-2" alt="face" /> David Grey </td>
-                        <td> Fund is not recieved </td>
-                        <td>
-                          <label className="badge badge-gradient-success">DONE</label>
-                        </td>
-                        <td> Dec 5, 2017 </td>
-                        <td> WD-12345 </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <img src={require("../../assets/images/faces/face2.jpg")} className="mr-2" alt="face" /> Stella Johnson </td>
-                        <td> High loading time </td>
-                        <td>
-                          <label className="badge badge-gradient-warning">PROGRESS</label>
-                        </td>
-                        <td> Dec 12, 2017 </td>
-                        <td> WD-12346 </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <img src={require("../../assets/images/faces/face3.jpg")} className="mr-2" alt="face" /> Marina Michel </td>
-                        <td> Website down for one week </td>
-                        <td>
-                          <label className="badge badge-gradient-info">ON HOLD</label>
-                        </td>
-                        <td> Dec 16, 2017 </td>
-                        <td> WD-12347 </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <img src={require("../../assets/images/faces/face4.jpg")} className="mr-2" alt="face" /> John Doe </td>
-                        <td> Loosing control on server </td>
-                        <td>
-                          <label className="badge badge-gradient-danger">REJECTED</label>
-                        </td>
-                        <td> Dec 3, 2017 </td>
-                        <td> WD-12348 </td>
-                      </tr>
-                    </tbody>
-                  </table>
-
-
-
-
-                  
+                <h4 className="card-title">Stocks In Portfolio</h4>
+                <Doughnut data={newTrafficData} />
+                <div id="traffic-chart-legend" className="rounded-legend legend-vertical legend-bottom-left pt-4">
+                    {this.state.liveData.map((stock)=> {
+                      return(
+                        <ul>
+                          <li> {stock.ticker} </li>
+                          <li> {stock.quantity} </li>
+                        </ul> );
+                      })} 
                 </div>
               </div>
             </div>
           </div>
-
-
-
-
-
-          
         </div>
       </div> 
     );
