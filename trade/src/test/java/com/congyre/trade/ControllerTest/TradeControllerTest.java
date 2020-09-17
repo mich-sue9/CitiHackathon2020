029a7ebcd396aef641c0c2dd697e9143e8fe1bb1
Michelle Lim <mich.sue9@gmail.com>
2020-09-17 18:13:01 -0400

    Set up for Trade Controller integration test

--- trade/src/test/java/com/congyre/trade/ControllerTest/TradeControllerTest.java
+++ trade/src/test/java/com/congyre/trade/ControllerTest/TradeControllerTest.java
@@ -1,10 +1,17 @@
 package com.congyre.trade.ControllerTest;
 
+import java.util.Date;
+
+import com.congyre.trade.entity.Trade;
+import com.congyre.trade.entity.Trade.TradeStatus;
+import com.congyre.trade.repository.TradeRepository;
 import com.congyre.trade.rest.TradeController;
 import com.congyre.trade.service.TradeService;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
@@ -27,6 +34,27 @@ public class TradeControllerTest {
 
     public static class Config{}
     
+    @Autowired 
+    private TradeController controller;
+
+    @Autowired 
+    private TradeRepository repo;
+
+    public String testID;
+
+    @Before
+    public void setUp(){
+        repo.deleteAll();
+        Trade trade = new Trade();
+        trade.setDateCreated(new Date());
+        trade.setStockTicker("AAPL");
+        trade.setQuantity(20);
+        trade.setRequestPrice(20.20);
+        trade.settStatus(TradeStatus.CREATED);
+
+        testID = repo.save(trade).getId().toString();
+    }
+
     @Test
     public void donothing(){}
     
