fbea5f035e39347407469a7903d1c7ba5f6a2a4e
Kexin Hu <kexin.hu@outlook.com>
2020-09-18 15:06:53 +0000

    added portfolio controler integrested tests

---
+++ trade/src/test/java/com/congyre/trade/ControllerTest/PortfolioStocksControllerTest.java
@@ -0,0 +1,129 @@
+package com.congyre.trade.ControllerTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.Date;
+import java.util.List;
+
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+import java.util.Optional;
+import com.congyre.trade.entity.Portfolio;
+import com.congyre.trade.entity.Trade;
+import com.congyre.trade.entity.Trade.TradeStatus;
+import com.congyre.trade.repository.PortfolioRepository;
+import com.congyre.trade.repository.TradeRepository;
+import com.congyre.trade.rest.PortfolioController;
+import com.congyre.trade.rest.TradeController;
+import com.congyre.trade.service.PortfolioService;
+import com.congyre.trade.service.TradeService;
+
+import org.bson.types.ObjectId;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes= PortfolioStocksControllerTest.Config.class)
+
+public class PortfolioStocksControllerTest {
+
+    @SpringBootApplication(scanBasePackageClasses = {
+        TradeController.class,
+        TradeService.class,
+        PortfolioService.class,
+    })
+    @EnableMongoRepositories(basePackages = "com.congyre.trade.repository")
+    @PropertySource("file:src/test/resources/test.properties")
+
+    public static class Config{}
+    
+    @Autowired 
+    private PortfolioController controller;
+
+    @Autowired 
+    private PortfolioRepository repo;
+
+    @Autowired
+    private PortfolioRepository portRepo;
+
+    public ObjectId testID = new ObjectId("5f46a3d545bee629d17fd7b2");
+
+
+    @Before
+    public void setUp(){
+        repo.deleteAll();
+        Trade trade = new Trade();
+        trade.setDateCreated(new Date());
+        trade.setStockTicker("AAPL");
+        trade.setQuantity(20);
+        trade.setRequestPrice(20.20);
+        trade.settStatus(TradeStatus.CREATED);
+
+        testID = ;
+    }
+
+    @Test
+    public void TestGetAllTrades(){
+        Iterable<Trade> trades = controller.getAllTrades();
+        Stream<Trade> stream = StreamSupport.stream(trades.spliterator(), false);
+        assertThat(stream.count(), equalTo(1L));
+    }
+
+    @Test
+    public void TestGetTradeById(){
+        Optional<Trade> trade = controller.getTradeById(testID);
+        assertThat(trade.isPresent(), equalTo(true));
+    }
+
+    @Test
+    public void TestGetTradesByTicker(){
+        List<Trade> trades = controller.getTradesByTicker("AAPL");
+        assertThat(trades.size(), equalTo(1));
+    }
+
+    @Test
+    public void TestAddTrade(){
+        //setup portfolio
+        String portId = "5f5ecf766bf9793a7412d8f1";
+        Portfolio testPort = new Portfolio();
+        testPort.setId(new ObjectId(portId));
+
+        //add portfolio into the repo
+        portRepo.save(testPort);
+
+        //test
+        controller.addTrade(new Trade(), portId);
+        Iterable<Trade> trades= repo.findAll();
+        Stream<Trade> stream = StreamSupport.stream(trades.spliterator(), false);
+        assertThat(stream.count(), equalTo(2L));
+    }
+
+
+    @Test
+    public void TestDeleteTradeById(){
+        controller.deleteTradeById(testID);
+        Iterable<Trade> trades= repo.findAll();
+        Stream<Trade> stream = StreamSupport.stream(trades.spliterator(), false);
+        assertThat(stream.count(), equalTo(0L));
+
+    }
+    
+    @Test
+    public void TestCancelTrade(){
+        controller.cancelTrade(testID);
+        Trade test= repo.findById(new ObjectId(testID)).orElse(null);
+        assertEquals(test.gettStatus(),TradeStatus.CANCELLED);
+    }
+
+}
